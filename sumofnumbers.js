145dd9a6fb7669b2ee6649d7507fcf8de33ef95b
Cory-Mac <school.coryparker@gmail.com>
2022-02-05 22:10:16 -1000

    update

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,39 +1,36 @@
-import * as _ from 'underscore'
+// 2 attempts, #1 DNF, #2 26.53
+// check for eslint
 
-function sumFor(numbers){
+function sumFor(numbers) {
   let total = 0;
-  for (let i=0; i<numbers.length;i++){
-    total+=numbers[i];
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
   }
   return total;
 }
 
-function sumWhile(numbers){
+function sumWhile(numbers) {
   let total = 0;
   let i = 0;
-  while (numbers.length > i){
-    total+=numbers[i];
+  while (numbers.length > i) {
+    total += numbers[i];
     i++;
   }
   return total;
 }
 
-function sumRecursion(numbers){
-
-  if (numbers.length === 0){
+function sumRecursion(numbers) {
+  if (numbers.length === 0) {
     return 0;
   }
-  else {
-    let data = numbers.pop;
-    return sumRecursion(data);
-  }
+  return numbers[0] + sumRecursion(numbers.slice(1, numbers.length));
 }
 
-function sumTheSimpleWay(numbers){
-  return _.reduce(numbers, function(memo, num){ return memo + num; });
+function sumTheSimpleWay(numbers) {
+  return _.reduce(numbers, function (memo, num) { return memo + num; });
 }
 
-let testData = [1,2,4,6];
+const testData = [1, 2, 4, 6];
 console.log(sumFor(testData));
 console.log(sumWhile(testData));
 console.log(sumRecursion(testData));
